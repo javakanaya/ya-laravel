bfd2608c876bd761f4547b24dc114851ad19b952
javakanaya <javakanaya@gmail.com>
2023-07-03 10:59:06 +0700

    simple views

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Java Kanaya",
+        "email" => "javakanaya@gmail.com",
+        "image" => "anjing.jpg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });

8a3f4cbad6d76250a8451603f122131a9649e636
javakanaya <javakanaya@gmail.com>
2023-07-03 15:30:04 +0700

    Blade templating engine

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.rtl.min.css"
+        integrity="sha384-PJsj/BTMqILvmcej7ulplguok8ag4xFTPryRq8xevL7eBYSmpXKcbNVuy+P0RMgq" crossorigin="anonymous">
+
+    <title>Java Blog | {{ $title }}</title>
+</head>
+
+<body>
+    {{-- directive template blade, ambil dari navbar di folder partials --}}
+    @include('partials.navbar')
+
+    <div class="container mt-4">
+        @yield('container')
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
+        "title" => "About",
         "name" => "Java Kanaya",
         "email" => "javakanaya@gmail.com",
         "image" => "anjing.jpg"
@@ -26,5 +29,49 @@ Route::get('/about', function () {
 });
 
 Route::get('/blog', function () {
-    return view('posts');
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Java Kanaya",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus excepturi magnam perspiciatis, dolorem eos omnis, dicta aut suscipit accusamus hic dolores laborum. Dicta mollitia veniam explicabo voluptate iste tempora cumque aut quasi ea, deleniti corporis ut. Libero, minus illum dolores eveniet dolor animi consectetur labore, quidem adipisci vel quia iusto non odio, suscipit iste error. Tempore nulla, reprehenderit quod dolore iste nam veritatis in rerum odit! Sit voluptatum vitae maxime, nam itaque odit exercitationem voluptatibus, est, ab iure ullam placeat!"
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Kanaya Prada",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus excepturi magnam perspiciatis, dolorem eos omnis, dicta aut suscipit accusamus hic dolores laborum. Dicta mollitia veniam explicabo voluptate iste tempora cumque aut quasi ea, deleniti corporis ut. Libero, minus illum dolores eveniet dolor animi consectetur labore, quidem adipisci vel quia iusto non odio, suscipit iste error. Tempore nulla, reprehenderit quod dolore iste nam veritatis in rerum odit! Sit voluptatum vitae maxime, nam itaque odit exercitationem voluptatibus, est, ab iure ullam placeat! Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus excepturi magnam perspiciatis, dolorem eos omnis, dicta aut suscipit accusamus hic dolores laborum. Dicta mollitia veniam explicabo voluptate iste tempora cumque aut quasi ea, deleniti corporis ut. Libero, minus illum dolores eveniet dolor animi consectetur labore, quidem adipisci vel quia iusto non odio, suscipit iste error. Tempore nulla, reprehenderit quod dolore iste nam veritatis in rerum odit! Sit voluptatum vitae maxime, nam itaque odit exercitationem voluptatibus, est, ab iure ullam placeat!"
+        ]
+    ];
+    return view('posts', [
+        "title" => "Posts",
+        "posts" => $blog_posts
+    ]);
+});
+
+Route::get('posts/{slug}', function ($slug) {
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Java Kanaya",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus excepturi magnam perspiciatis, dolorem eos omnis, dicta aut suscipit accusamus hic dolores laborum. Dicta mollitia veniam explicabo voluptate iste tempora cumque aut quasi ea, deleniti corporis ut. Libero, minus illum dolores eveniet dolor animi consectetur labore, quidem adipisci vel quia iusto non odio, suscipit iste error. Tempore nulla, reprehenderit quod dolore iste nam veritatis in rerum odit! Sit voluptatum vitae maxime, nam itaque odit exercitationem voluptatibus, est, ab iure ullam placeat!"
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Kanaya Prada",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus excepturi magnam perspiciatis, dolorem eos omnis, dicta aut suscipit accusamus hic dolores laborum. Dicta mollitia veniam explicabo voluptate iste tempora cumque aut quasi ea, deleniti corporis ut. Libero, minus illum dolores eveniet dolor animi consectetur labore, quidem adipisci vel quia iusto non odio, suscipit iste error. Tempore nulla, reprehenderit quod dolore iste nam veritatis in rerum odit! Sit voluptatum vitae maxime, nam itaque odit exercitationem voluptatibus, est, ab iure ullam placeat! Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus excepturi magnam perspiciatis, dolorem eos omnis, dicta aut suscipit accusamus hic dolores laborum. Dicta mollitia veniam explicabo voluptate iste tempora cumque aut quasi ea, deleniti corporis ut. Libero, minus illum dolores eveniet dolor animi consectetur labore, quidem adipisci vel quia iusto non odio, suscipit iste error. Tempore nulla, reprehenderit quod dolore iste nam veritatis in rerum odit! Sit voluptatum vitae maxime, nam itaque odit exercitationem voluptatibus, est, ab iure ullam placeat!"
+        ]
+    ];
+    $new_post = [];
+    foreach($blog_posts as $post) {
+        if($post['slug'] === $slug) {
+            $new_post = $post;
+        }
+    }
+    return view('post', [
+        "title" => "Single Post",
+        "post" =>$new_post
+    ]);
 });

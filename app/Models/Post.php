d43e00c6140d99d6d8ac12e8c541f693595eeb01
javakanaya <javakanaya@gmail.com>
2023-07-06 15:08:11 +0700

    Eloquent Relationship

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -12,4 +12,10 @@ class Post extends Model
     // biar tahu, field mana saja yang boleh diisi. (pilih salah satu)
     // protected $fillable = ['title', 'excerpt', 'body']; // ini yang boleh create/update
     protected $guarded = ['id']; // ini yang ga boleh di create/update
+
+    public function category()
+    {
+        // models post berelasi dengan model kategori
+        return $this->belongsTo(Category::class);
+    }
 }
--- config/app.php
+++ config/app.php
@@ -184,6 +184,7 @@ return [
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
         'Post' => App\Models\Post::class,
+        'Category' => App\Models\Category::class,
     ])->toArray(),
 
 ];

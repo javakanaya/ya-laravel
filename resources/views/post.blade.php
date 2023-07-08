28e3e51bc82513b550c05f8ce369bbc368a9225d
javakanaya <javakanaya@gmail.com>
2023-07-08 18:58:33 +0700

    fix: category

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,10 +3,14 @@
 @section('container')
     <h1 class="mb-5">{{ $post->title }}</h1>
 
-    <p>By: <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a
-            href="/categories/{{ $post->category->slug }} "class="text-decoration-none">{{ $post->category->name }}</a></p>
+    <p>
+        By: 
+        <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> 
+        in 
+        <a href="/categories/{{ $post->category->slug }} "class="text-decoration-none">{{ $post->category->name }}</a>
+    </p>
 
     {!! $post->body !!} {{-- biar bisa tetep baca tag html --}}
 
-    <a href="/posts" class="mt-5 pb-5 text-decoration-none">Back to Posts</a>
+    <p><a href="/posts" class="mt-5 pb-5 text-decoration-none">Back to Posts</a></p>
 @endsection

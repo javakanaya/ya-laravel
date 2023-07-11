2092ce7238a1558d8531a26d74a7d713f64d79ff
javakanaya <javakanaya@gmail.com>
2023-07-11 22:47:29 +0700

    User registrasion

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -12,5 +14,28 @@ class RegisterController extends Controller
             'title' => 'Register',
             'active' => 'register'
         ]);
+    }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|max:255',
+            'username' => ['required', 'min:3', 'max:255', 'unique:users'],
+            'email' => 'required|email:dns|unique:users',
+            'password' => 'required|min:5|max:255'
+        ]);
+
+        // Pakai bcrypt
+        // $validatedData['password'] = bcrypt($validatedData['password']);
+
+        // Pakai Hashing
+        $validatedData['password'] = Hash::make($validatedData['password']);
+        
+        User::create($validatedData);
+
+        // $request->session()->flash('success', 'Resgistration successful! Please login');
+        // $request->session()->$request->flash();
+
+        return redirect('/login')->with('success', 'Resgistration successful! Please login');
     }
 }

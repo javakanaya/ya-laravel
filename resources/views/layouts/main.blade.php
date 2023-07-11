7618ba32d95ba2e4bb17f335d2244975fc4d18fe
javakanaya <javakanaya@gmail.com>
2023-07-11 18:55:07 +0700

    Login Resgister views

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -9,6 +9,12 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+
+    {{-- Bootsrap Icons --}}
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+
+    {{-- My Styles --}}
+    <link rel="stylesheet" href="/css/style.css">
     <title>Java Blog | {{ $title }}</title>
 </head>
 

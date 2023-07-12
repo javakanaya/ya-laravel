e93455a9e57f1727ac977b0b55bc4ed04185501d
javakanaya <javakanaya@gmail.com>
2023-07-12 09:01:57 +0700

    User login

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -11,5 +12,32 @@ class LoginController extends Controller
             'title' => 'Login',
             'active' => 'login'
         ]);
+    }
+
+    public function authenticate(Request $request) {
+        $credentials = $request->validate([
+            'email' => 'required|email:dns',
+            'password' => 'required'
+        ]);
+
+        if(Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            // pakai intended biar lewat middleware
+            return redirect()->intended('/dashboard');
+        }
+
+        // mengirim ke flah message
+        return back()->with('loginError', 'Login failed!');
+    }
+
+    public function logout(Request $request) {
+        Auth::logout();
+
+        $request->session()->invalidate();
+ 
+        $request->session()->regenerateToken();
+     
+        return redirect('/');
     }
 }
